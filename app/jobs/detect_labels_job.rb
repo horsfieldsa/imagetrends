ca2619b23849da926f264d5302e399420d97e9f2
Scott Hosfield <horsfieldsa@gmail.com>
2018-10-18 10:08:10 -0700

    Adds start recording to detect labels job

--- app/jobs/detect_labels_job.rb
+++ app/jobs/detect_labels_job.rb
@@ -3,46 +3,57 @@ class DetectLabelsJob
     require 'aws-xray-sdk'
   
     def perform(sneaker_id)
-        begin
 
-            segment = XRay.recorder.begin_segment 'imagetrends'
+        segment = XRay.recorder.begin_segment 'imagetrends'
 
-            detection_logger.info("Attempting to detect labels for Image #{sneaker_id}")
-            @sneaker = Sneaker.find(sneaker_id)
+        annotations = {
+            image: sneaker_id
+          }
+          
+        segment.annotations.update annotations
 
-            client = Aws::Rekognition::Client.new
-            resp = client.detect_labels({
-                    image: { bytes: @sneaker.sneaker_image.download }, 
-                    max_labels: 20, 
-                    min_confidence: 50,
-            })
+        XRay.recorder.capture('detect_labels') do |subsegment|
 
-            if resp.labels.count == 0 
-                detection_logger.info("No labels detected for Image: #{sneaker_id}")
+            begin
+
+                detection_logger.info("Attempting to detect labels for Image #{sneaker_id}")
+                @sneaker = Sneaker.find(sneaker_id)
+
+                client = Aws::Rekognition::Client.new
+                resp = client.detect_labels({
+                        image: { bytes: @sneaker.sneaker_image.download }, 
+                        max_labels: 20, 
+                        min_confidence: 50,
+                })
+
+                if resp.labels.count == 0 
+                    detection_logger.info("No labels detected for Image: #{sneaker_id}")
+                end
+
+                resp.labels.each do |label|
+                    puts "#{label.name}-#{label.confidence.to_i}"
+
+                    @tag = Tag.new
+                    @tag.name = label.name
+                    @tag.confidence = label.confidence
+                    @tag.source = "Rekognition - Detect Labels"
+                    @tag.sneaker = @sneaker
+                    @tag.save
+
+                    detection_logger.info("Label detected for Image: #{sneaker_id} Name: #{@tag.name} Confidence: #{@tag.confidence}")
+
+                end
+                rescue StandardError => e
+                    puts("--------------------------------- [ERROR] ---------------------------------")
+                    puts(e)
+                    @tag = Tag.new
+                    @tag.name = "Error"
+                    @tag.source = "Rekognition - Detect Labels"
+                    @tag.sneaker = @sneaker
+                    @tag.save
+                    detection_logger.error("Error detecting labels for Image: #{sneaker_id} Details: #{e}")
+
             end
-
-            resp.labels.each do |label|
-                puts "#{label.name}-#{label.confidence.to_i}"
-
-                @tag = Tag.new
-                @tag.name = label.name
-                @tag.confidence = label.confidence
-                @tag.source = "Rekognition - Detect Labels"
-                @tag.sneaker = @sneaker
-                @tag.save
-
-                detection_logger.info("Label detected for Image: #{sneaker_id} Name: #{@tag.name} Confidence: #{@tag.confidence}")
-
-            end
-            rescue StandardError => e
-                puts("--------------------------------- [ERROR] ---------------------------------")
-                puts(e)
-                @tag = Tag.new
-                @tag.name = "Error"
-                @tag.source = "Rekognition - Detect Labels"
-                @tag.sneaker = @sneaker
-                @tag.save
-                detection_logger.error("Error detecting labels for Image: #{sneaker_id} Details: #{e}")
 
             XRay.recorder.end_segment
         end
