5b47f2456dd68741f52de2f1438622860960e9b7
Scott Hosfield <horsfieldsa@gmail.com>
2018-10-23 13:26:46 -0700

    Fixes tag query to remove unnecessary join

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,7 +6,7 @@ class TagsController < ApplicationController
   # GET /tags
   # GET /tags.json
   def index
-    @tags_distinct = Tag.group(:name).joins(:image)
+    @tags_distinct = Tag.group(:name)
     tag_logger.info("Loading a list of tags: #{@tags_distinct.count}")
   end
 
