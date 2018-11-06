2e0251432085baad1ee982630007db31a7cc2af6
Scott Hosfield <horsfieldsa@gmail.com>
2018-11-05 21:09:02 -0800

    Changes production storage provider to amazon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
